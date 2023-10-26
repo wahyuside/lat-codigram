0e0de1841fb1f0cd3ec2afbbd58d2b85e3ce9edb
Wahyu Said <boxout.wsn@gmail.com>
2023-10-26 12:26:04 +0700

    adding mode users function

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,7 @@
-const { users } = require("../models");
+const { users, posts } = require("../models");
 
 class UserController {
-  static async getUsers(req, res) {
+  static async getAllUsers(req, res) {
     try {
       let result = await users.findAll();
       console.log(result);
@@ -10,14 +10,27 @@ class UserController {
       res.status(500).json("Error " + error);
     }
   }
+  static async getUser(req, res) {
+    try {
+      let id = +req.params.id;
+
+      const result = await users.findByPk(id);
+      console.log(result);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json("Error " + error);
+    }
+  }
   static async register(req, res) {
     try {
-      const { username, email, password, imgprf } = req.body;
+      const { username, email, password, imgprf, role } = req.body;
+      console.log([req.body]);
       let result = await users.create({
         username,
         email,
         password,
-        img,
+        imgprf,
+        role,
       });
       res.status(201).json(result);
     } catch (error) {
@@ -25,9 +38,32 @@ class UserController {
     }
   }
 
+  static async editUser(req, res) {
+    try {
+      const { username, email, password, imgprf, role } = req.body;
+      let id = +req.params.id;
+      let result = await users.update(
+        { username, email, password, imgprf, role },
+        { where: { id: id } },
+        { returning: true }
+      );
+      console.log([req.body]);
+      result[0] === 1
+        ? res.status(200).json({
+            message: `Post id: ${id} has been updated.`,
+          })
+        : res.status(400).json({
+            message: `Post id: ${id} has not been updated.`,
+          });
+    } catch (error) {
+      res.status(500).json("Error " + error);
+    }
+  }
+
   static async delUser(req, res) {
     try {
-      let result = await users.destroy();
+      let id = +req.params.id;
+      let result = await users.destroy({ where: { id: id } });
       res.status(201).json(result);
     } catch (error) {
       res.status(500).json("Error " + error);
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,10 @@
 const userRoutes = require("express").Router();
 const userController = require("../controllers/UserController");
 
-userRoutes.get("/", userController.getUsers);
+userRoutes.get("/list", userController.getAllUsers);
+userRoutes.get("/:id", userController.getUser);
 userRoutes.post("/register", userController.register);
+userRoutes.put("/edit/:id", userController.editUser);
+userRoutes.delete("/delete/:id", userController.delUser);
 
 module.exports = userRoutes;
