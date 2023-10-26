335007ba3076dbc71a0c8576a722df1b68805241
Wahyu Said <boxout.wsn@gmail.com>
2023-10-26 13:58:20 +0700

    adding functions to posts

---
+++ server/controllers/PostController.js
@@ -0,0 +1,72 @@
+const { users, posts } = require("../models");
+
+class PostController {
+  static async getAllPosts(req, res) {
+    try {
+      let result = await posts.findAll();
+      console.log(result);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json("Error " + error);
+    }
+  }
+  static async getPost(req, res) {
+    try {
+      let id = +req.params.id;
+
+      const result = await posts.findByPk(id, { include: [users] });
+      console.log(result);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json("Error " + error);
+    }
+  }
+  static async createPost(req, res) {
+    try {
+      const { imgpost, caption, userID, status } = req.body;
+      console.log([req.body]);
+      let result = await posts.create({
+        imgpost,
+        caption,
+        userID,
+        status,
+      });
+      res.status(201).json(result);
+    } catch (error) {
+      res.status(500).json("Error " + error);
+    }
+  }
+
+  static async editPost(req, res) {
+    try {
+      const { imgpost, caption, userID, status } = req.body;
+      let id = +req.params.id;
+      let result = await posts.update(
+        { imgpost, caption, userID, status },
+        { where: { id: id } }
+      );
+      console.log([req.body]);
+      result[0] === 1
+        ? res.status(200).json({
+            message: `Post id: ${id} has been updated.`,
+          })
+        : res.status(400).json({
+            message: `Post id: ${id} has not been updated.`,
+          });
+    } catch (error) {
+      res.status(500).json("Error " + error);
+    }
+  }
+
+  static async delPost(req, res) {
+    try {
+      let id = +req.params.id;
+      let result = await posts.destroy({ where: { id: id } });
+      res.status(201).json(result);
+    } catch (error) {
+      res.status(500).json("Error " + error);
+    }
+  }
+}
+
+module.exports = PostController;
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,4 +1,10 @@
 const postRoutes = require("express").Router();
 const postController = require("../controllers/PostController");
 
+postRoutes.get("/", postController.getAllPosts);
+postRoutes.get("/:id", postController.getPost);
+postRoutes.post("/create", postController.createPost);
+postRoutes.put("/update/:id", postController.editPost);
+postRoutes.delete("/delete/:id", postController.delPost);
+
 module.exports = postRoutes;
